6788b850c1638c7f543d38e71437cae45de1284b
Collin Glass <collinglass@gmx.com>
2013-11-04 12:37:33 -0500

    add implementation to board.h

--- board.h
+++ board.h
@@ -1,14 +1,26 @@
+#include <vector>
+#include <iostream>
+
+using std::vector;
+
 template <class T>
 class Board {
 	T** tiles;
 public:
-	Board(int cols, int rows){
+	Board(int cols, int rows);
+    void placeTile(char col, int row, T element);
+    T getTileAt(char col, int row);
+    vector<T> getAdjacent(char col, int row);
+};
+
+	template<class T> Board<T>::Board(int cols, int rows){
 		tiles = new T[rows];
 		for (char i = 0; i < rows; i++ ) {
 			tiles[i] = new T[cols];
 		}
 	}
-    void placeTile(char col, int row, T element){
+
+	template<class T> void Board<T>::placeTile(char col, int row, T element){
     	if ( col >= 97 && col <= 122 ) {
     		col -= 97;
     		tiles[col][row] = element;
@@ -20,20 +32,39 @@ public:
     	}
     	return;
     }
-    T getTileAt(char col, int row){
-    	T temp;
+
+	template<class T> T Board<T>::getTileAt(char col, int row){
+    	T result;
     	if ( col >= 97 && col <= 122 ) {
     		col -= 97;
-    		temp = tiles[col][row];
+    		result = tiles[col][row];
     	} else if ( col >= 65 && col <= 90 ) {
     		col -= 65;
-    		temp = tiles[col][row];
+    		result = tiles[col][row];
     	} else {
     		std::cout << "Invalid Column Input!" << std::endl;
     	}
-    	return temp;
+    	return result;
     }
-    vector<T> getAdjacent(char col, int row){	 // up to 4 in the list
 
+    template<class T> vector<T> Board<T>::getAdjacent(char col, int row){	 // up to 4 in the list
+    	vector<T> result;
+    	if ( col >= 97 && col <= 122 ) {
+    		col -= 97;
+    		result.push_back(tiles[col-1][row]);
+    		result.push_back(tiles[col+1][row]);
+    		result.push_back(tiles[col][row-1]);
+    		result.push_back(tiles[col][row+1]);
+    	} else if ( col >= 65 && col <= 90 ) {
+    		col -= 65;
+    		result.push_back(tiles[col-1][row]);
+    		result.push_back(tiles[col+1][row]);
+    		result.push_back(tiles[col][row-1]);
+    		result.push_back(tiles[col][row+1]);
+    	} else {
+    		std::cout << "Invalid Column Input!" << std::endl;
+    	}
+    	return result;
     }
-};
+
+
