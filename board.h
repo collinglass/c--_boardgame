34abd7c09539c8fe004688295b727f4c29b66d64
Collin Glass <collinglass@gmx.com>
2013-11-04 11:52:35 -0500

    Start board.h

---
+++ board.h
@@ -0,0 +1,17 @@
+template <class T>
+class Board {
+	T** tiles;
+public:
+	Board(int cols, int rows){
+
+	}
+    placeTile(char, int, T){
+
+    }
+    T getTileAt(char, int){
+
+    }
+    vector<T> getAdjacent(char, int){	 // up to 4 in the list
+
+    }
+};
